049fa3869d958e47f3c79b71abc495f6a60540b2
NewEgoDocs <juhan211@naver.com>
2024-02-17 11:22:21 +0900

    ca-product seperate module

---
+++ build.gradle.kts
@@ -0,0 +1,123 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+plugins {
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.4"
+    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.22"
+    kotlin("plugin.jpa") version "1.9.22"
+    kotlin("plugin.allopen") version "1.9.22"
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
+
+subprojects {
+
+    apply {
+        plugin("java")
+        plugin("org.springframework.boot")
+        plugin("io.spring.dependency-management")
+        plugin("org.jlleitschuh.gradle.ktlint")
+        plugin("kotlin")
+        plugin("kotlin-kapt")
+        plugin("kotlin-spring")
+        plugin("kotlin-jpa")
+        plugin("kotlin-allopen")
+    }
+
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+        implementation("org.springframework.boot:spring-boot-starter-validation")
+
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation("io.github.microutils:kotlin-logging:1.12.5")
+
+        runtimeOnly("com.mysql:mysql-connector-j")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+    }
+
+    allOpen {
+        annotation("jakarta.persistence.Entity")
+        annotation("jakarta.persistence.Embeddable")
+        annotation("jakarta.persistence.MappedSuperclass")
+    }
+
+    group = "io.sharing.server.product"
+    version = "0.0.1-SNAPSHOT"
+    java.sourceCompatibility = JavaVersion.VERSION_17
+
+    configurations {
+        compileOnly {
+            extendsFrom(configurations.annotationProcessor.get())
+        }
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "17"
+        }
+    }
+
+    tasks.withType<Test> {
+        maxHeapSize = "4096m"
+        useJUnitPlatform()
+        dependsOn(tasks.ktlintCheck)
+    }
+
+    configure<KtlintExtension> {
+        verbose.set(true)
+        disabledRules.addAll("import-ordering", "no-wildcard-imports", "filename", "indent", "parameter-list-wrapping")
+    }
+}
+
+
+
+project(":core") {
+    dependencies {
+    }
+
+    tasks.getByName<Jar>("jar") { enabled = true }
+    tasks.getByName<BootJar>("bootJar") { enabled = false }
+}
+
+project(":api") {
+    val snippetsDir = file("build/generated-snippets")
+
+    dependencies {
+        implementation(project(":core"))
+        // Other dependencies (make sure to remove Spring REST Docs related ones)
+    }
+
+    tasks.withType<BootJar> {
+        archiveFileName.set("sharing-api-product.jar")
+    }
+
+    tasks.register<Zip>("zip") {
+        dependsOn("bootJar")
+    }
+
+    // If the Test task had configurations specific to REST Docs, review and adjust them
+    tasks.withType<Test> {
+        // Adjust configurations if necessary
+        outputs.dir(snippetsDir)
+    }
+
+}
+
+
---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+rootProject.name = "product"
+
+include("core")
+include("api")
